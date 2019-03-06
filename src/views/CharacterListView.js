0263a986c3c256be44a9d857d73f0795a5195f40
Joel Bartlett <joel@murbar.com>
2019-03-06 16:13:50 -0500

    Render message during fetch

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -17,6 +17,7 @@ class CharacterListView extends React.Component {
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return <div>Fetching data...</div>;
     }
     return (
       <div className="CharactersList_wrapper">
